c78e8f49bfc390cef2641fd86cd4bb0fa797eb8a
Rina-Scr <muntyanu.kairna@yandex.ru>
2023-01-18 00:39:41 +0300

    Добавлен пакет MaterialDesignThemes

---
+++ BankApp/ViewModel/Base/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BankApp.ViewModel.Base
+{
+    abstract internal class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T> (ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if(Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
